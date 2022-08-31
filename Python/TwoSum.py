094a2f6eb5607840b2b7e7d9f0a293b3d13fce27
KathyP825 <kp2327@nyu.edu>
2022-08-31 10:13:00 -0400

    fixed spacing on TwoSum

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -2,7 +2,7 @@
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
-
+------------------------------------------------------------------------
 Example 1:
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
@@ -15,21 +15,18 @@ Output: [1,2]
 Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
- 
-
+------------------------------------------------------------------------
 Constraints:
 2 <= nums.length <= 104
 -109 <= nums[i] <= 109
 -109 <= target <= 109
 Only one valid answer exists.
- 
 
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 '''
 
+# Solution with O(n^2)
 class Solution(object):
-
-    # Solution with O(n^2)
     def twoSum(self, nums, target):
         for i in range(len(nums) - 1):
             for j in range(i + 1, len(nums)):
